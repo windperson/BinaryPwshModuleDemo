e7484450692ff49ff10bb125460912172f0b3a28
Isak Pao <windperson@gmail.com>
2024-10-20 19:11:10 +0800

    Add source files

---
+++ PwshStandardModule/FavoriteStuff.cs
@@ -0,0 +1,9 @@
+namespace PwshStandardModule
+{
+
+    public class FavoriteStuff
+    {
+        public int FavoriteNumber { get; set; }
+        public string FavoritePet { get; set; }
+    }
+}
---
+++ csharp_pwsh_module/MyPowerShellCmdlet.cs
@@ -0,0 +1,12 @@
+﻿using System.Management.Automation;
+
+namespace csharp_pwsh_module;
+
+[Cmdlet(VerbsCommon.Get, "Hello")]
+public class MyPowerShellCmdlet : Cmdlet
+{
+    protected override void ProcessRecord()
+    {
+        WriteObject("Hello, C# Cmdlet!");
+    }
+}
