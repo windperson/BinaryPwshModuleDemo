8f67a072cf511e3ad1ad31a29f01821ad52dfdd1
Isak Pao <windperson@gmail.com>
2024-12-06 05:52:55 +0800

    Add minimal binary cmdlet unit test case

---
+++ tests/MinimalPwshModuleDemo.Test/MyPowerShellCmdletTests.cs
@@ -0,0 +1,18 @@
+﻿namespace MinimalPwshModuleDemo.Test;
+
+public class MyPowerShellCmdletTests
+{
+    [Fact]
+    public void ProcessRecord_ShouldWriteHelloMessage()
+    {
+        // Arrange
+        var cmdlet = new MyPowerShellCmdlet();
+
+        // Act
+        var output = cmdlet.Invoke().OfType<string>().ToList();
+
+        // Assert
+        Assert.Single(output);
+        Assert.Equal("Hello, C# Cmdlet!", output[0]);
+    }
+}
